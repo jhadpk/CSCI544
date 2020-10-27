3c085d9f3f682457c1af4487fedb494282d1f9d0
Deepak Jha <deepakjha@Deepaks-MacBook-Pro.local>
2020-10-26 19:10:14 -0700

    assignment 5

--- Assignment5/hmmdecode.py
+++ Assignment5/hmmdecode.py
@@ -11,13 +11,21 @@ def learn_model(file):
     return model
 
 
+def get_emission_probability(observation):
+    if word not in trained_words:
+        #if word is not seen, let transition prob decide of the top N tags
+        return 1
+    else:
+        return 0 if observation not in emission_probability_dict else emission_probability_dict[observation]
+
+
 def get_zero_matrix(rows, cols):
     return [[0 for x in range(cols)] for y in range(rows)]
 
 
 def get_max_probability_final_state(words, viterbi_prob_matrix):
     max_probability = -1
-    for state_index in range(total_states):
+    for state_index in range(len(unique_states)):
         current_probability = viterbi_prob_matrix[state_index][len(words) - 1]
         if max_probability < current_probability:
             max_probability = current_probability
@@ -34,90 +42,84 @@ def get_predicted_tags_by_backtracking(words, viterbi_prob_matrix, parent_info_m
     index_col = len(words) - 1
     while index_col > 0:
         index_row = parent_info_matrix[index_row][index_col]
-        tag = unique_states[index_row]
-        predicted_tags = [tag] + predicted_tags
+        predicted_tags = [unique_states[index_row]] + predicted_tags
         index_col = index_col - 1
     return predicted_tags
 
 
-def get_predicted_tags(words, prediction):
+def get_tagged_line(words, prediction):
     tagged_line = ""
     for state_index, input_word in enumerate(words):
         tagged_line += input_word + "/" + prediction[state_index] + " "
     return tagged_line.strip()
 
 
-testing_file = sys.argv[1]
-# testing_file = "hmm-training-data/it_isdt_dev_raw.txt"
+# testing_file = sys.argv[1]
+testing_file = "hmm-training-data/it_isdt_dev_raw.txt"
 model_file = "hmmmodel.txt"
 output_file = "hmmoutput.txt"
 
 model = learn_model(model_file)
-
-transitions = ast.literal_eval(model.get("transitions"))
-transition_states = ast.literal_eval(model.get("transition_states"))
-emissions = ast.literal_eval(model.get("emissions"))
-emission_states = ast.literal_eval(model.get("emission_states"))
-initial_states = ast.literal_eval(model.get("initial_states"))
-total_lines = int(model.get("total_lines"))
-unique_words = ast.literal_eval(model.get("unique_words"))
-unique_states = ast.literal_eval(model.get("unique_states"))
-total_states = int(model.get("total_states"))
-unique_words_per_tag = ast.literal_eval(model.get("unique_words_per_tag"))
 transition_probability_dict = ast.literal_eval(model.get("transition_probability_dict"))
 emission_probability_dict = ast.literal_eval(model.get("emission_probability_dict"))
+first_word_states_probabilities = ast.literal_eval(model.get("first_word_states_probabilities"))
+unique_words_per_state = ast.literal_eval(model.get("unique_words_per_state"))
+unique_words = ast.literal_eval(model.get("unique_words"))
+unique_states = ast.literal_eval(model.get("unique_states"))
+total_lines = int(model.get("total_lines"))
 
 trained_words = set()
 for word in unique_words:
     trained_words.add(word)
+
+first_word_states = set()
+for state in first_word_states_probabilities:
+    first_word_states.add(state)
+
+most_frequent_states = set()
+for state in unique_words_per_state:
+    most_frequent_states.add(state)
+    if len(most_frequent_states) == 5:
+        break
 
 output_file_write = open(output_file, 'w')
 
 test_lines = open(testing_file, encoding="utf8", errors='ignore').read().splitlines()
 for line in test_lines:
     words = line.split()
-    viterbi_prob_matrix = get_zero_matrix(total_states, len(words))
-    parent_info_matrix = get_zero_matrix(total_states, len(words))
+    viterbi_prob_matrix = get_zero_matrix(len(unique_states), len(words))
+    parent_info_matrix = get_zero_matrix(len(unique_states), len(words))
     for word_index in range(len(words)):
-        for state_index in range(total_states):
-            current_state = unique_states[state_index]
+        states_to_iterate = unique_states
+        if words[word_index] not in trained_words:
+            #for unseen word, using top 5 most frequent tags for prediction
+            states_to_iterate = most_frequent_states
+        for index, current_state in enumerate(states_to_iterate):
             word = words[word_index]
             observation = word + "/" + current_state
+            emission_probability = get_emission_probability(observation)
+
+            state_index = index if word in trained_words else unique_states.index(current_state)
+
             if word_index == 0:
-                if current_state not in initial_states:
-                    viterbi_prob_matrix[state_index][word_index] = 0
-                else:
-                    if word not in trained_words:
-                        emission_probability = 1
-                    else:
-                        emission_probability = 0 if observation not in emission_probability_dict else emission_probability_dict[observation]
-                    viterbi_prob_matrix[state_index][word_index] = emission_probability * (initial_states[current_state] / total_lines)
+                viterbi_prob_matrix[state_index][word_index] = 0 if current_state not in first_word_states else emission_probability * first_word_states_probabilities[current_state]
             else:
-                transition_prob_statewise = []
                 max_transition_prob = -1
                 max_transition_prob_previous_state_index = -1
-                for unique_states_index, previous_state in enumerate(unique_states):
+                for previous_state_index, previous_state in enumerate(unique_states):
                     transition = previous_state + "->" + current_state
-                    if viterbi_prob_matrix[unique_states_index][word_index - 1] == 0:
+                    if viterbi_prob_matrix[previous_state_index][word_index - 1] == 0:
                         current_prob = 0
                     else:
-                        transition_probability = 1 / (transition_states[previous_state] + total_states) if transition not in transition_probability_dict else transition_probability_dict[transition]
-                        current_prob = viterbi_prob_matrix[unique_states_index][word_index - 1] * transition_probability
+                        if transition not in transition_probability_dict:
+                            transition = previous_state + "->UNKNOWN"
+                        transition_probability = transition_probability_dict[transition]
+                        current_prob = viterbi_prob_matrix[previous_state_index][word_index - 1] * transition_probability
                     if current_prob > max_transition_prob:
                         max_transition_prob = current_prob
-                        max_transition_prob_previous_state_index = unique_states_index
+                        max_transition_prob_previous_state_index = previous_state_index
                 parent_info_matrix[state_index][word_index] = max_transition_prob_previous_state_index
-                if max_transition_prob == 0:
-                    viterbi_prob_matrix[state_index][word_index] = 0
-                else:
-                    if word not in trained_words:
-                        emission_probability = 1
-                    else:
-                        emission_probability = 0 if observation not in emission_probability_dict else \
-                            emission_probability_dict[observation]
-                    viterbi_prob_matrix[state_index][word_index] = 0 if emission_probability == 0 else emission_probability * max_transition_prob
+                viterbi_prob_matrix[state_index][word_index] = 0 if max_transition_prob == 0 else emission_probability * max_transition_prob
     prediction = get_predicted_tags_by_backtracking(words, viterbi_prob_matrix, parent_info_matrix)
-    tagged_line = get_predicted_tags(words, prediction)
+    tagged_line = get_tagged_line(words, prediction)
     output_file_write.write(tagged_line + "\n")
-
-
--- Assignment5/hmmlearn.py
+++ Assignment5/hmmlearn.py
@@ -1,12 +1,15 @@
 from collections import Counter
 import sys
 
-
 def get_transition_probabilities(transitions, transitions_states, total_states):
     transition_probabilities = {}
     for transition in transitions:
         source = transition.split("->")[0]
         transition_probabilities[transition] = (transitions[transition] + 1) / (transitions_states[source] + total_states)
+    #append missing transitions
+    for state in unique_states:
+        transition = state + "->UNKNOWN"
+        transition_probabilities[transition] = 1 / (transition_states[state] + len(unique_states))
     return transition_probabilities
 
 
@@ -18,40 +21,41 @@ def get_emission_probabilities(emissions, emission_states):
     return emission_probabilities
 
 
-def create_model(file_name, transitions, transition_states, emissions, emission_states, q0, total_lines,
-                 unique_words, unique_states, unique_words_per_tag, transition_probability, emission_probability):
+def get_first_word_state_probabilities():
+    first_word_states_probabilities = {}
+    for state in first_word_states:
+        first_word_states_probabilities[state] = first_word_states[state] / total_lines
+    return first_word_states_probabilities
+
+
+def create_model(file_name, transition_probability, emission_probability, first_word_states_probabilities, unique_words_per_state, unique_words, unique_states, total_lines):
     with open(file_name, 'w') as f:
-        f.write("transitions=" + str(dict(transitions)) + "\n")
-        f.write("transition_states=" + str(dict(transition_states)) + "\n")
-        f.write("emissions=" + str(dict(emissions)) + "\n")
-        f.write("emission_states=" + str(dict(emission_states)) + "\n")
-        f.write("initial_states=" + str(dict(q0)) + "\n")
-        f.write("total_lines=" + str(total_lines) + "\n")
-        f.write("unique_words=" + str(unique_words) + "\n")
-        f.write("unique_states=" + str(unique_states) + "\n")
-        f.write("total_states=" + str(len(unique_states)) + "\n")
-        f.write("unique_words_per_tag=" + str(dict(unique_words_per_tag)) + "\n")
         f.write("transition_probability_dict=" + str(dict(transition_probability)) + "\n")
         f.write("emission_probability_dict=" + str(dict(emission_probability)) + "\n")
+        f.write("first_word_states_probabilities=" + str(dict(first_word_states_probabilities)) + "\n")
+        f.write("unique_words_per_state=" + str(dict(unique_words_per_state)) + "\n")
+        f.write("unique_words=" + str(unique_words) + "\n")
+        f.write("unique_states=" + str(unique_states) + "\n")
+        f.write("total_lines=" + str(total_lines) + "\n")
 
 
-initial_states = []
+# training_file = sys.argv[1]
+training_file = "hmm-training-data/it_isdt_train_tagged.txt"
+
+first_word_states = []
 emissions = []
 observations = []
 emission_states = []
 transitions = []
 transition_states = []
+unique_words_per_state = {}
 
-unique_words_per_tag = {}
-# training_file = sys.argv[1]
-training_file = "hmm-training-data/it_isdt_train_tagged.txt"
+training_lines = open(training_file, encoding="utf8", errors='ignore').read().splitlines()
 
-f = open(training_file, encoding="utf8", errors='ignore').read().splitlines()
-
-for line in f:
+for line in training_lines:
     words = line.split()
     emissions += words
-    initial_states.append(words[0].split("/")[-1])  # first word state
+    first_word_states.append(words[0].split("/")[-1])  # first word state
     for i, word in enumerate(words):
         word_and_tag = word.rsplit("/", 1)
         observation = word_and_tag[0]
@@ -61,29 +65,28 @@ for line in f:
         if i < len(words) - 1:
             transition_states.append(state)
             transitions.append(state + "->" + words[i+1].rsplit("/", 1)[1])
-        if state in unique_words_per_tag:
-            if observation not in unique_words_per_tag[state]:
-                current = unique_words_per_tag[state]
+        if state in unique_words_per_state:
+            if observation not in unique_words_per_state[state]:
+                current = unique_words_per_state[state]
                 current.add(observation)
-                unique_words_per_tag[state] = current
+                unique_words_per_state[state] = current
         else:
-            unique_words_per_tag[state] = set()
-for x in unique_words_per_tag:
-    total_unique_words = len(unique_words_per_tag[x])
-    unique_words_per_tag[x] = total_unique_words
-unique_words_per_tag = {k: v for k, v in sorted(unique_words_per_tag.items(), key=lambda item: -item[1])}
+            unique_words_per_state[state] = set()
+for x in unique_words_per_state:
+    total_unique_words = len(unique_words_per_state[x])
+    unique_words_per_state[x] = total_unique_words
+unique_words_per_state = {k: v for k, v in sorted(unique_words_per_state.items(), key=lambda item: -item[1])}
 
-
-total_lines = len(f)
 unique_words = list(set(observations))
 unique_states = list(set(emission_states))
+total_lines = len(training_lines)
 transitions = Counter(transitions)
 transition_states = Counter(transition_states)
 emissions = Counter(emissions)
 emission_states = Counter(emission_states)
-initial_states = Counter(initial_states)
 transition_probability = get_transition_probabilities(transitions, transition_states, len(unique_states))
 emission_probability = get_emission_probabilities(emissions, emission_states)
+first_word_states = Counter(first_word_states)
+first_word_states_probabilities = get_first_word_state_probabilities()
 
-create_model("hmmmodel.txt", transitions, transition_states, emissions, emission_states, initial_states, total_lines,
-             unique_words, unique_states, unique_words_per_tag, transition_probability, emission_probability)
+create_model("hmmmodel.txt", transition_probability, emission_probability, first_word_states_probabilities, unique_words_per_state, unique_words, unique_states, total_lines)
